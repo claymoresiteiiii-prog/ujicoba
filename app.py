b01618cf3a1a8926f12b4047ceb998627f044300
claymoresiteiiii-prog <claymoresiteiiii@gmail.com>
2026-04-13 17:09:52 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import openpyxl
+
+st.set_page_config(layout="wide", page_title="Dashboard Tanam Singkong")
